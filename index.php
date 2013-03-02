920acf47d14f50c449ee9d2c954e8d6d8086515a
asperous <nospam@asperous.us>
2013-03-02 00:37:59 -0800

    Moved logic

--- index.php
+++ index.php
@@ -5,16 +5,47 @@ $smarty = new Smarty();
 $smarty->setTemplateDir('./');
 $smarty->setCompileDir('libs/compile/');
 $smarty->setCacheDir('libs/cache');
-$smarty->caching = 1;
+$smarty->caching = 2;
 $smarty->cache_lifetime = 86400;
 
-$birthday = new DateTime(date("Y") . "-08-14");
-$birthday = $birthday->getTimestamp();
-$christmas = new DateTime(date("Y") . "-12-25");
-$christmas = $christmas->getTimestamp();
-$seconds_in_a_day = 86400;
-
-
-$smarty->assign("days_until_birthday", ($birthday - time()) / $seconds_in_a_day);
-$smarty->assign("days_until_christmas", ($christmas - time()) / $seconds_in_a_day);
+if (!$smarty->isCached('index.tpl')) {
+    // Birthday & Christmas seconds
+    $birthday = new DateTime(date("Y") . "-08-14");
+    $birthday = $birthday->getTimestamp();
+    $christmas = new DateTime(date("Y") . "-12-25");
+    $christmas = $christmas->getTimestamp();
+    $seconds_in_a_day = 86400;
+    $smarty->assign("days_until_birthday", floor(($birthday - time()) / $seconds_in_a_day));
+    $smarty->assign("days_until_christmas", floor(($christmas - time()) / $seconds_in_a_day));
+    // Steam & Reddit
+    require('libs/simple_html_dom.php');
+    $html = file_get_html("http://www.reddit.com/user/asperous");
+    $result = $html->find(".karma");
+    $smarty->assign("reddit", $result[0]->innertext."|".$result[1]->innertext);
+    $html = file_get_html("http://steamcommunity.com/id/asperous/games");
+    $json = $html->find("script", -2)->innertext;
+    $json = substr($json, strpos($json, "[{"), strpos($json, "}];")-14);
+    $steam_data = json_decode($json, "true");
+    $smarty->assign("dota", $steam_data[0]['hours_forever']);
+    // Featured Files
+    $output = array();
+    $files =  scandir("./featured");
+    rsort($files);
+    foreach ($files as $file) {
+        if($file == "." or $file == "..") continue;
+        $data = explode("_", $file);
+        if (count($data) == 2) {
+            $clean = $data[1];
+            $description = "";
+        }
+        else {
+            $clean = $data[2];
+            $description = $data[1];
+        }
+        $clean = str_replace( "~", "/", $clean);
+        $clean = str_replace( ".png", "", $clean);
+        $output[$file] = array($clean, $description);
+    }
+    $smarty->assign("files", $output);
+}
 $smarty->display('index.tpl');
--- libs/compile/1dfb6d298930cfbbe7fcc2824c930c4965e662f1.file.index.tpl.php
+++ libs/compile/1dfb6d298930cfbbe7fcc2824c930c4965e662f1.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.8, created on 2012-05-09 20:40:43
+<?php /* Smarty version Smarty-3.1.8, created on 2013-03-02 00:23:31
          compiled from ".\index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:301514fab332118c6b6-45710148%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '1dfb6d298930cfbbe7fcc2824c930c4965e662f1' => 
     array (
       0 => '.\\index.tpl',
-      1 => 1336621183,
+      1 => 1362212225,
       2 => 'file',
     ),
   ),
@@ -19,9 +19,10 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'unifunc' => 'content_4fab3321300750_95888053',
   'variables' => 
   array (
-    'feed' => 0,
-    'steam' => 0,
-    'karma' => 0,
+    'days_until_birthday' => 0,
+    'days_until_christmas' => 0,
+    'dota' => 0,
+    'reddit' => 0,
     'files' => 0,
     'data' => 0,
     'file' => 0,
@@ -31,20 +32,14 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <?php if ($_valid && !is_callable('content_4fab3321300750_95888053')) {function content_4fab3321300750_95888053($_smarty_tpl) {?><?php if (!is_callable('smarty_modifier_spacify')) include 'C:\\Users\\a5c\\Desktop\\ab\\Apps\\xampp\\htdocs\\libs\\plugins\\modifier.spacify.php';
 if (!is_callable('smarty_block_markdown')) include 'C:\\Users\\a5c\\Desktop\\ab\\Apps\\xampp\\htdocs\\libs\\plugins\\block.markdown.php';
 if (!is_callable('smarty_function_mailto')) include 'C:\\Users\\a5c\\Desktop\\ab\\Apps\\xampp\\htdocs\\libs\\plugins\\function.mailto.php';
-if (!is_callable('smarty_function_json')) include 'C:\\Users\\a5c\\Desktop\\ab\\Apps\\xampp\\htdocs\\libs\\plugins\\function.json.php';
-if (!is_callable('smarty_modifier_linkify')) include 'C:\\Users\\a5c\\Desktop\\ab\\Apps\\xampp\\htdocs\\libs\\plugins\\modifier.linkify.php';
-if (!is_callable('smarty_modifier_truncate')) include 'C:\\Users\\a5c\\Desktop\\ab\\Apps\\xampp\\htdocs\\libs\\plugins\\modifier.truncate.php';
-if (!is_callable('smarty_function_scrapeweb')) include 'C:\\Users\\a5c\\Desktop\\ab\\Apps\\xampp\\htdocs\\libs\\plugins\\function.scrapeweb.php';
-if (!is_callable('smarty_function_jsondata')) include 'C:\\Users\\a5c\\Desktop\\ab\\Apps\\xampp\\htdocs\\libs\\plugins\\function.jsondata.php';
 if (!is_callable('smarty_function_featuredfiles')) include 'C:\\Users\\a5c\\Desktop\\ab\\Apps\\xampp\\htdocs\\libs\\plugins\\function.featuredfiles.php';
-?><?php echo '<?xml';?> version="1.0" encoding="UTF-8"<?php echo '?>';?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
         <!-- Meta -->
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta name="description" content="<?php echo htmlspecialchars("-=======:]>>>>>", ENT_QUOTES, 'UTF-8', true);?>
-" />
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta name="description" content="Andy Chase from Tigard, Oregon" />
         <link rel="openid.server" href="http://asperous.us/openid/index.php" /> 
         <link rel="openid.delegate" href="http://asperous.us/openid/index.php" />
         <link rel="pavatar" href="http://asperous.us/openid/pavatar.png" />
@@ -55,8 +50,7 @@ if (!is_callable('smarty_function_featuredfiles')) include 'C:\\Users\\a5c\\Desk
         <script type="text/javascript" src="http://use.typekit.com/eyu2uhm.js"></script>
         <script type="text/javascript">try{ Typekit.load();}catch(e){ }</script>
         
-        <!-- Styles -->
-        <link type="text/css" rel="stylesheet" href="./static/less.css" />
+        <!-- Style -->
         <link type="text/css" rel="stylesheet" href="./static/sitestyle.css" />
     </head>
     
@@ -72,49 +66,44 @@ if (!is_callable('smarty_function_featuredfiles')) include 'C:\\Users\\a5c\\Desk
 <?php $_smarty_tpl->smarty->_tag_stack[] = array('markdown', array()); $_block_repeat=true; echo smarty_block_markdown(array(), null, $_smarty_tpl, $_block_repeat);while ($_block_repeat) { ob_start();?>
 
 ## About me ##
-Andy Chase  
-[Tigard, Or](http://en.wikipedia.org/wiki/Tigard,_Oregon)  
+Andy Chase <br />
+[Tigard, Or](http://en.wikipedia.org/wiki/Tigard,_Oregon) <br />
 <?php echo smarty_function_mailto(array('text'=>"theandychase<br/>@gmail.com",'address'=>"theandychase@gmail.com",'encode'=>'javascript'),$_smarty_tpl);?>
   
-<br />
-[[LinkedIn]](http://www.linkedin.com/pub/andy-chase/51/304/30a)  
-[[Facebook]](http://facebook.com/asper)  
-[[PDF] Resume](http://dl.dropbox.com/u/27512039/resume.pdf)  
+
 <br />
 
-<?php ob_start();?><?php echo preg_replace('!\s+!u', '',"http://api.twitter.com/1/statuses/user_timeline.json
-            ?exclude_replies=true
-            &oauth_token=14402880-qIo9493d6OKOwHFyeJ8r5OkbD8TGk9qMsrw1aUUhs
-            &screen_name=asperous");?>
-<?php $_tmp1=ob_get_clean();?><?php echo smarty_function_json(array('var'=>"feed",'url'=>$_tmp1),$_smarty_tpl);?>
-
-<?php ob_start();?><?php echo preg_replace('!\s+!u', ' ',$_smarty_tpl->tpl_vars['feed']->value[0]['text']);?>
-<?php $_tmp2=ob_get_clean();?><?php if ($_tmp2!=''){?>
-<i><?php echo smarty_modifier_linkify($_smarty_tpl->tpl_vars['feed']->value[0]['text']);?>
- on <?php echo smarty_modifier_truncate($_smarty_tpl->tpl_vars['feed']->value[0]['created_at'],12,'');?>
-</i>
+[[LinkedIn]](http://www.linkedin.com/pub/andy-chase/51/304/30a)  <br />
+[[Facebook]](http://facebook.com/asper)  <br /><br />
+<?php if ($_smarty_tpl->tpl_vars['days_until_birthday']->value<30&&$_smarty_tpl->tpl_vars['days_until_birthday']->value>1){?>
+*It's almost my birthday!*
+<?php }elseif($_smarty_tpl->tpl_vars['days_until_birthday']->value==0){?>
+*It's my birthday!*
+<?php }elseif($_smarty_tpl->tpl_vars['days_until_christmas']->value<40&&$_smarty_tpl->tpl_vars['days_until_christmas']->value>1){?>
+*<?php echo $_smarty_tpl->tpl_vars['days_until_christmas']->value;?>
+ day<?php if ($_smarty_tpl->tpl_vars['days_until_christmas']->value==1){?>s<?php }?> until Christmas!*
+<?php }elseif(floor($_smarty_tpl->tpl_vars['days_until_christmas']->value)==0){?>
+*It's Christmas!*
 <?php }?>
-[[Twitter]](http://twitter.com/asperous)
-   
+
+[[Amzn] Wishlist](http://amzn.com/w/3RA2F8HIILNSL)
+
 <br />
-<?php echo smarty_function_scrapeweb(array('url'=>"http://www.reddit.com/user/asperous/",'find'=>".karma",'var'=>"karma"),$_smarty_tpl);?>
 
-<?php echo smarty_function_scrapeweb(array('url'=>"http://steamcommunity.com/id/asperous/games",'find'=>"script",'var'=>"steam"),$_smarty_tpl);?>
+[[Twitter]](http://twitter.com/asperous) <br />
 
-<?php ob_start();?><?php echo substr($_smarty_tpl->tpl_vars['steam']->value[4]->innertext,15);?>
-<?php $_tmp3=ob_get_clean();?><?php $_smarty_tpl->tpl_vars["steam"] = new Smarty_variable($_tmp3, null, 0);?>
-<?php echo smarty_function_jsondata(array('var'=>"steam",'data'=>$_smarty_tpl->tpl_vars['steam']->value,'tag'=>"hours_forever"),$_smarty_tpl);?>
-
-
-
+<br />
 [[Hypem] fav. music](http://hypem.com/asperous)
 <br /><br />
-[[Steam]<br /><?php echo $_smarty_tpl->tpl_vars['steam']->value;?>
-  Dota2 Hrs.](http://steamcommunity.com/id/asperous)  
-[[Reddit] <?php echo $_smarty_tpl->tpl_vars['karma']->value[0]->innertext;?>
-|<?php echo $_smarty_tpl->tpl_vars['karma']->value[1]->innertext;?>
-  Karma](http://www.reddit.com/user/asperous/)  
-<br /><br />
+[[Steam]
+    <?php echo $_smarty_tpl->tpl_vars['dota']->value;?>
+ <br /> Dota2 Hrs.](http://steamcommunity.com/id/asperous)
+
+[[Reddit]
+    <?php echo $_smarty_tpl->tpl_vars['reddit']->value;?>
+<br />  Karma](http://www.reddit.com/user/asperous/)
+
+<br /><br /> 
 <?php $_block_content = ob_get_clean(); $_block_repeat=false; echo smarty_block_markdown(array(), $_block_content, $_smarty_tpl, $_block_repeat);  } array_pop($_smarty_tpl->smarty->_tag_stack);?>
 
 
@@ -126,10 +115,13 @@ Andy Chase
 <?php $_smarty_tpl->smarty->_tag_stack[] = array('markdown', array()); $_block_repeat=true; echo smarty_block_markdown(array(), null, $_smarty_tpl, $_block_repeat);while ($_block_repeat) { ob_start();?>
 
 ## My work ##
-[[HazardDigital] Proj.s](http://hazarddigital.com/ourwork)  
-[[YouTube] Movies](http://youtube.com/user/asperous)  
-[[Bitbucket] Code](https://bitbucket.org/asperous)  
-[[Picasa] Pictures](https://picasaweb.google.com/116933133784637904048)  
+[[YouTube] Movies](http://youtube.com/user/asperous) <br />
+[[Github] Code](https://github.com/asperous) <br />
+[[Flickr] Pics](http://www.flickr.com/photos/asperous/sets/)
+<br /><br />
+[R&#233;sum&#233;](http://dl.dropbox.com/u/27512039/resume.pdf)
+
+<br /><br /> 
 <?php $_block_content = ob_get_clean(); $_block_repeat=false; echo smarty_block_markdown(array(), $_block_content, $_smarty_tpl, $_block_repeat);  } array_pop($_smarty_tpl->smarty->_tag_stack);?>
 
 
